c008956b4ce78ff3f531645b92dbdeb995fad995
lorenzo famiglini <47078991+lorenzofamiglini@users.noreply.github.com>
2023-05-10 17:37:55 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='calfram',
+    version='0.1.0',
+    url='https://github.com/yourusername/calfram',
+    author='Your Name',
+    author_email='lorenzofamiglini@gmail.com',
+    description='A comprehensive framework for assessing calibration in machine and deep learning models',
+    packages=find_packages(),
+    install_requires=[],
+)
