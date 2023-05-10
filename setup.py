890a30dc670f1bb32f60259293ea1441fec10d8b
devsmudilab <dev.mudilab@gmail.com>
2023-05-10 18:02:58 +0100

    pypi updates

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='calfram',
     version='0.1.0',
-    url=https://github.com/lorenzofamiglini/CalFram',
+    url='https://github.com/lorenzofamiglini/CalFram',
     author='Lorenzo Famiglini',
     author_email='lorenzofamiglini@gmail.com',
     description='A comprehensive framework for assessing calibration in machine and deep learning models',
