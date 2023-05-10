3a2c93f926fbfed26de7a9a484157a8e4a61ee68
lorenzo famiglini <47078991+lorenzofamiglini@users.noreply.github.com>
2023-05-10 17:41:12 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,16 @@ from setuptools import setup, find_packages
 setup(
     name='calfram',
     version='0.1.0',
-    url='https://github.com/yourusername/calfram',
-    author='Your Name',
+    url=https://github.com/lorenzofamiglini/CalFram',
+    author='Lorenzo Famiglini',
     author_email='lorenzofamiglini@gmail.com',
     description='A comprehensive framework for assessing calibration in machine and deep learning models',
     packages=find_packages(),
-    install_requires=[],
+    install_requires=[
+    'pandas',
+    'numpy',
+    'matplotlib',
+    'sklearn',
+    'ipdb',
+]
 )
